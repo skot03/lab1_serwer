b7b9d7964c2b3b8887785f755f2a13d3f7b632b7
skot03 <skotnickipawe@gmail.com>
2024-10-25 09:44:39 +0200

    first commit

--- nginx/ui_app/requests_async.js
+++ nginx/ui_app/requests_async.js
@@ -1,35 +1,98 @@
-// requests.js
+function newDiv(responseData) {
+    let responseContainer = document.querySelector('.members');
+    let response_item = document.createElement('div');
+    response_item.classList.add('item');
+    responseContainer.appendChild(response_item);
 
-// Function to send GET request
+    let info = document.createElement('div');
+    info.classList.add('info');
+    response_item.appendChild(info);
+
+    let response_fullname = document.createElement('div');
+    response_fullname.textContent = responseData['first_name'] + ' ' + responseData['last_name'];
+    response_fullname.classList.add('fullname');
+    info.appendChild(response_fullname);
+
+    let response_role = document.createElement('div');
+    response_role.textContent = responseData['role'];
+    response_role.classList.add('response-role');
+    info.appendChild(response_role);
+
+
+    let delete_img = document.createElement('img');
+    delete_img.src = 'delete.png';
+    delete_img.classList.add('delete-img');
+    delete_img.addEventListener('click', () => {
+        response_item.remove();
+        sendDeleteRequest(responseData);
+    });
+    response_item.appendChild(delete_img);
+   
+    
+    
+}
+
 async function sendGetRequest() {
-    const input = document.getElementById("getInputField").value;
-    const url = `http://localhost:8000/${input}`; // Assumes the server is running on localhost:8000
-
     try {
-        const response = await fetch(url);
-        const data = await response.json();
-        document.getElementById("getResponse").textContent = JSON.stringify(data, null, 2);
+        const response = await fetch('http://localhost:8000/');
+        const responseData = await response.json();
+        responseData.forEach((data) => {
+            newDiv(data);
+        });
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
-// Function to send POST request
-async function sendPostRequest() {
-    const name = document.getElementById("postNameField").value;
-    const data = { name: name };
-
+async function sendPostRequest(data) {
     try {
-        const response = await fetch("http://localhost:8000/", {
-            method: "POST",
+        const response = await fetch('http://localhost:8000/', {
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json"
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         });
         const responseData = await response.json();
-        document.getElementById("postResponse").textContent = JSON.stringify(responseData, null, 2);
+        newDiv(responseData);
     } catch (error) {
         console.error('Error:', error);
     }
 }
+
+async function sendDeleteRequest(data) {
+    try {
+        const response = await fetch('http://localhost:8000/', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    sendGetRequest();
+
+    let myform = document.getElementById('myform');
+    if (myform) {
+        myform.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            console.log('myform submission started');
+            const data = { 
+                first_name: document.getElementById('first_name').value, 
+                last_name: document.getElementById('last_name').value,
+                role: document.getElementById('role').value
+            };
+            sendPostRequest(data);
+        })
+    }
+    else {
+        console.error('myform not found');
+    }
+});
